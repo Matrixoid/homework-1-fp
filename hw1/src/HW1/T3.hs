1db3e008dda24e1fb46f9f4946b9b333fdc0e24e
Lord Matrixoid <mr.art.pro@mail.ru>
2021-10-25 11:50:35 +0300

    T3adv

--- hw1/src/HW1/T3.hs
+++ hw1/src/HW1/T3.hs
@@ -13,7 +13,28 @@ data Tree a = Leaf
             deriving(Show, Eq)
 
 mkbranch :: Tree a -> a -> Tree a -> Tree a
-mkbranch left element right = Branch (tsize left + tsize right + 1) left element right
+mkbranch Leaf element Leaf = Branch 1 Leaf element Leaf
+mkbranch (Branch n1 left1 element1 right1) element Leaf
+  | tdepth tree1 > 2 = Branch (n1 + 1) left1 element1 (mkbranch right1 element Leaf)
+  | otherwise        = Branch (n1 + 1) tree1 element Leaf
+  where
+    tree1 = Branch n1 left1 element1 right1
+mkbranch Leaf element (Branch n2 left2 element2 right2)
+  | tdepth tree2 > 2 = Branch (n2 + 1) (mkbranch Leaf element left2) element2 right2
+  | otherwise        = Branch (n2 + 1) Leaf element tree2
+  where
+    tree2 = Branch n2 left2 element2 right2
+mkbranch (Branch n1 left1 element1 right1) element (Branch n2 left2 element2 right2)
+  | tdepth tree1 - tdepth tree2 >= 2    = Branch tSize left1 element1  mkbranchRight
+  | tdepth tree1 - tdepth tree2 <= (-2) = Branch tSize mkbranchLeft element2 right2
+  | otherwise                           = Branch tSize tree1 element tree2
+  where
+    tree1         = Branch n1 left1 element1 right1
+    tree2         = Branch n2 left2 element2 right2
+    mkbranchLeft  = mkbranch tree1 element left2
+    mkbranchRight = mkbranch right1 element tree2
+    tSize = n1 + n2 + 1
+--mkbranch left element right = Branch (tsize left + tsize right + 1) left element right
 
 tsize :: Tree a -> Int
 tsize Leaf             = 0
