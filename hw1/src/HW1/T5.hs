aa7171fc6fa1d68fe2e80b199f02fb1dba3c4249
Lord Matrixoid <mr.art.pro@mail.ru>
2021-10-27 02:51:47 +0300

    HW1T53

--- hw1/src/HW1/T5.hs
+++ hw1/src/HW1/T5.hs
@@ -1,1 +1,13 @@
 module HW1.T5 where
+  
+import Data.List.NonEmpty as NonEmpty
+
+splitOn :: Eq a => a -> [a] -> NonEmpty [a]
+splitOn sep = foldr f ([] :| [])
+  where
+    f delim list@(head :| tail)
+      | sep /= delim = (delim : head) :| tail
+      |otherwise     = cons [] list 
+      
+--joinWith :: a -> NonEmpty [a] -> [a]
+joinWith delim = foldl1 (\first second -> first ++ [delim] ++ second)
