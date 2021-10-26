966dbf9dcde49af02e9c53b2f698d1765341416b
Lord Matrixoid <mr.art.pro@mail.ru>
2021-10-26 23:50:24 +0300

    HW1T52

--- hw1/src/HW1/T5.hs
+++ hw1/src/HW1/T5.hs
@@ -4,15 +4,17 @@ module HW1.T5
   ) where
 
 import Data.List.NonEmpty
+import Data.Foldable
 
 splitOn :: Eq a => a -> [a] -> NonEmpty [a]
-splitOn separator = foldr splitOn' ([] :| [])
+splitOn separator = Data.Foldable.foldr splitOn' ([] :| [])
   where
     splitOn' sep (x :| xs)
       | separator == sep = cons [] (x :| xs)
       | otherwise        = (sep : x) :| xs
 
 joinWith :: Eq a => a -> NonEmpty [a] -> [a]
-joinWith separator = foldl1 joinWith'
+joinWith separator list = joinWith' [] separator list
   where
-    joinWith' str1 str2 = str1 ++ [separator] ++ str2
+    joinWith' acc sep (x : xs) = joinWith' (acc ++ x ++ [separator]) sep xs
+    joinWith' acc sep (x : []) = acc ++ x
