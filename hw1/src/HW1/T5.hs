856536bf09dd9e31f3a764ca1f5ade73be7ecd33
Lord Matrixoid <mr.art.pro@mail.ru>
2021-10-26 23:36:09 +0300

    HW1 T5

--- hw1/src/HW1/T5.hs
+++ hw1/src/HW1/T5.hs
@@ -1,1 +1,18 @@
-module HW1.T5 where
+module HW1.T5
+  ( splitOn
+  , joinWith
+  ) where
+
+import Data.List.NonEmpty
+
+splitOn :: Eq a => a -> [a] -> NonEmpty [a]
+splitOn separator = foldr splitOn' ([] :| [])
+  where
+    splitOn' sep (x :| xs)
+      | separator == sep = cons [] (x :| xs)
+      | otherwise        = (sep : x) :| xs
+
+joinWith :: Eq a => a -> NonEmpty [a] -> [a]
+joinWith separator = foldl1 joinWith'
+  where
+    joinWith' str1 str2 = str1 ++ [separator] ++ str2
