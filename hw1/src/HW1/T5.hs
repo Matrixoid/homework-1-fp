91adda0a07b5fa0a0a224f0c5e333a184c5ff7fb
Lord Matrixoid <mr.art.pro@mail.ru>
2021-10-26 22:36:32 +0300

    HW1whitoutT5

--- hw1/src/HW1/T5.hs
+++ hw1/src/HW1/T5.hs
@@ -1,18 +1,1 @@
 module HW1.T5 where
-
-import Data.List.NonEmpty
-
-splitOn :: Eq a => a -> [a] -> NonEmpty [a]
-splitOn separator = foldr (split separator) ([] :| [])
-  where
-    split :: Eq a => a -> a -> NonEmpty [a] -> NonEmpty [a]
-    split sep current (x :| xs)
-      | current == sep = [] :| (x : xs)
-      | otherwise      = (current : x) :| xs
-
-joinWith :: Eq a => a -> NonEmpty [a] -> [a]
-joinWith separator = foldr (join separator) []
-  where
-    join :: Eq a => a -> [a] -> [a] -> [a]
-    join _ current []     = current
-    join sep current list = current ++ (sep : list)
